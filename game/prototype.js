32f448a4cbf8e8513b80eb9801ba92b7db4e5858
CaboozledPie <andrewprasetya05@gmail.com>
2025-08-19 14:52:30 -0700

    added basic skin

--- game/prototype.js
+++ game/prototype.js
@@ -1,10 +1,40 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
+let mouseX = 400, mouseY = 400;
+let mouseIsClicked = false;
+
+// grab mouseX, mouseY
+canvas.addEventListener("mousemove",
+    e => {
+        const rect = canvas.getBoundingClientRect();
+        mouseX = e.clientX - rect.left;
+        mouseY = e.clientY - rect.top;
+    }
+);
+
+// handle mouseIsClicked
+canvas.addEventListener("mousedown", () => mouseIsClicked = true);
+canvas.addEventListener("mouseup", () => mouseIsClicked = false);
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "blue";
-    ctx.fillRect(50, 50, 50, 50);
+
+    ctx.beginPath();
+    if (mouseIsClicked) {
+        ctx.fillStyle = "blue";
+    }
+    else {
+        ctx.fillStyle = "red";
+    }
+    ctx.fillRect(mouseX, mouseY, 50, 50);
+    ctx.fill();
+    ctx.closePath();
 }
 
-draw();
+function loop() {
+    draw();
+    requestAnimationFrame(loop);
+}
+
+loop();
