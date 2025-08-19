a9d36013fd192f0b9858b55942e639487c554a65
CaboozledPie <andrewprasetya05@gmail.com>
2025-08-19 16:50:21 -0700

    big updates to prototype

--- game/prototype.js
+++ game/prototype.js
@@ -1,8 +1,11 @@
+import {Tile, Board} "from board.js";
+
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 let mouseX = 400, mouseY = 400;
-let mouseIsClicked = false;
+let mouseIsPressed = false;
+let mouseIsReleased = false;
 
 // grab mouseX, mouseY
 canvas.addEventListener("mousemove",
@@ -13,28 +16,40 @@ canvas.addEventListener("mousemove",
     }
 );
 
-// handle mouseIsClicked
-canvas.addEventListener("mousedown", () => mouseIsClicked = true);
-canvas.addEventListener("mouseup", () => mouseIsClicked = false);
+// handle mouseIsPressed
+canvas.addEventListener("mousedown", () => mouseIsPressed = true);
+canvas.addEventListener("mouseup", function() {
+    mouseIsPressed = false;
+    mouseIsReleased = true;
+});
+
+/** ACTUALLY RUNNING THE GAME **/
+// even this behavior should probably be sectioned off later but for now it's going in here
+let boardShape = [
+    [1, 1, 1, 1],
+    [1, 1, 1, 1],
+    [1, 1, 1, 1],
+    [1, 1, 1, 1],
+];
+var wordBoard = new Board(boardShape);
+wordBoard.generateLetters();
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     ctx.beginPath();
-    if (mouseIsClicked) {
-        ctx.fillStyle = "blue";
-    }
-    else {
-        ctx.fillStyle = "red";
-    }
-    ctx.fillRect(mouseX, mouseY, 50, 50);
     ctx.fill();
     ctx.closePath();
 }
 
 function loop() {
+    if (mouseIsReleased) {
+        wordBoard.clearGuess();
+    }
+    
     draw();
     requestAnimationFrame(loop);
+    mouseIsReleased = false; // i have to do this for this logic to work, no getting around it
 }
 
 loop();
